e9ecbfaf3e1017f21ae22d8b8c10f946b360708a
Leon de Boer <ldeboer@gateway.net.au>
2017-07-15 04:51:56 +0800

    Update Pi2.bat

--- USB_HID/Pi2.bat
+++ USB_HID/Pi2.bat
@@ -1,11 +1,11 @@
 @REM COMPILER COMMAND LINE
-g:\pi\gcc_pi_6_2\bin\arm-none-eabi-gcc -Wall -O2 -mfpu=neon -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -nostartfiles -specs=nosys.specs -ffunction-sections -Wl,-gc-sections -Wl,-T,rpi.ld main.c smartstart.S usb.c -o kernel.elf -lc -lm
+g:\pi\gcc_pi_6_3\bin\arm-none-eabi-gcc -Wall -O2 -mfpu=neon -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -nostartfiles -specs=nosys.specs -ffunction-sections -Wl,-gc-sections -Wl,-T,rpi.ld main.c smartstart.S usb.c -o kernel.elf -lc -lm
 @echo off
 if %errorlevel% EQU 1 (goto build_fail)
 
 @REM LINKER COMMAND LINE
 @echo on
-g:\pi\gcc_pi_6_2\bin\arm-none-eabi-objcopy kernel.elf -O binary kernel7.img
+g:\pi\gcc_pi_6_3\bin\arm-none-eabi-objcopy kernel.elf -O binary kernel7.img
 @echo off
 if %errorlevel% EQU 1 (goto build_fail) 
 echo BUILD COMPLETED NORMALLY
