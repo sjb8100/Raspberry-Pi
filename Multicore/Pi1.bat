f722e55521407d5661be417f21175891bb049ffd
Leon de Boer <ldeboer@gateway.net.au>
2017-08-07 17:35:35 +0800

    Update Pi1.bat

--- Multicore/Pi1.bat
+++ Multicore/Pi1.bat
@@ -1,5 +1,5 @@
 @REM COMPILER COMMAND LINE
-g:\pi\gcc_pi_6_3\bin\arm-none-eabi-gcc -Wall -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostdlib -nostartfiles -specs=nosys.specs -ffreestanding -fno-common -Wl,-gc-sections -Wl,--build-id=none -Wl,-T,rpi32.ld main.c smartstart32.S rpi-BasicHardware.c printf.c -o kernel.elf -lc -lm -lgcc
+g:\pi\gcc_pi_6_3\bin\arm-none-eabi-gcc -Wall -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostdlib -nostartfiles -ffreestanding -fno-common -Wl,-gc-sections -Wl,--build-id=none -Wl,-T,rpi32.ld main.c smartstart32.S rpi-BasicHardware.c printf.c -o kernel.elf -lc -lm -lgcc
 @echo off
 if %errorlevel% EQU 1 (goto build_fail)
 
