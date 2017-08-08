d2eb87f1312a3aa398f5ff2eff88efeccd37b3ae
Leon de Boer <ldeboer@gateway.net.au>
2017-08-08 23:03:28 +0800

    Add files via upload

--- Multicore/Pi2.bat
+++ Multicore/Pi2.bat
@@ -2,10 +2,10 @@
 @echo off
 set "bindir=g:\pi\gcc_pi_6_3\bin\"
 set "cflags=-Wall -O2 -mfpu=neon -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -nostdlib -nostartfiles -ffreestanding --specs=nosys.specs"
-set "sflags=-ffunction-sections -fdata-sections -fomit-frame-pointer -fno-builtin -fno-common -fno-pic -fno-asynchronous-unwind-tables"
-set "linkflags=-Wno-address -Wl,-gc-sections -Wl,-Bdynamic -Wl,-Map,kernel.map -Wl,--build-id=none"
-set "outflags=-Wa,-a >list.txt -o kernel.elf"
-set "libflags=-lc -lg -lm -lgcc -lnosys"
+set "sflags=-fomit-frame-pointer -fno-common -fno-asynchronous-unwind-tables"
+set "linkflags=-Wl,--print-gc -Wl,-Bdynamic -Wl,-Map,kernel.map -Wl,--build-id=none"
+set "outflags=-Wa,-a=list.txt -o kernel.elf"
+set "libflags=-lc -lg -lm -lgcc"
 @echo on
 %bindir%arm-none-eabi-gcc  %cflags% %sflags% %linkflags% -Wl,-T,rpi32.ld main.c  SmartStart32.S rpi-BasicHardware.c %outflags% %libflags%
 @echo off
