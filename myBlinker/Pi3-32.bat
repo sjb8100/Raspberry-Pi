55a38ccf04906b4366df9efa4c4cb384f56b7f99
Leon de Boer <ldeboer@gateway.net.au>
2017-09-21 05:10:22 +0800

    Add files via upload

--- myBlinker/Pi3-32.bat
+++ myBlinker/Pi3-32.bat
@@ -1,19 +1,19 @@
 @REM COMPILER COMMAND LINE
 @echo off
 set "bindir=g:\pi\gcc_pi_6_3\bin\"
-set "cpuflags=-Wall -O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -mno-unaligned-access -fno-exceptions"
+set "cpuflags=-Wall -O3 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv8-a -mtune=cortex-a53 -mno-unaligned-access -fno-tree-loop-vectorize -fno-tree-slp-vectorize"
 set "asmflags=-nostdlib -nostartfiles -ffreestanding -fno-asynchronous-unwind-tables -fomit-frame-pointer -Wa,-a>list.txt"
 set "linkerflags=-Wl,-gc-sections -Wl,--build-id=none -Wl,-Bdynamic -Wl,-Map,kernel.map"
 set "outflags=-o kernel.elf"
-set "libflags=-lc -lm -lg -lgcc"
+set "libflags=-lc -lm -lgcc -lnosys"
 @echo on
-%bindir%arm-none-eabi-gcc %cpuflags% %asmflags% %linkerflags% -Wl,-T,rpi32.ld main.c SmartStart32.S rpi-SmartStart.C emb-stdio.c %outflags% %libflags%
+%bindir%arm-none-eabi-gcc %cpuflags% %asmflags% %linkerflags% -Wl,-T,rpi32.ld main.c SmartStart32.S rpi-SmartStart.c emb-stdio.c %outflags% %libflags% 
 @echo off
 if %errorlevel% EQU 1 (goto build_fail)
 
 @REM LINKER COMMAND LINE
 @echo on
-%bindir%arm-none-eabi-objcopy kernel.elf -O binary DiskImg/kernel.img
+%bindir%arm-none-eabi-objcopy kernel.elf -O binary DiskImg\kernel8-32.img
 @echo off
 if %errorlevel% EQU 1 (goto build_fail) 
 echo BUILD COMPLETED NORMALLY
--- myBlinker/main.c
+++ myBlinker/main.c
@@ -1,8 +1,7 @@
 #include <stdbool.h>		// C standard needed for bool
 #include <stdint.h>			// C standard for uint8_t, uint16_t, uint32_t etc
-#include <stdio.h>				// Needed for printf
+#include "emb-stdio.h"		// Needed for printf
 #include "rpi-smartstart.h"		
-#include "rpi-BasicHardware.h"
 
 
 static bool lit = false;
@@ -11,43 +10,21 @@ void c_irq_handler(void) {
 	set_Activity_LED(lit);											// Turn LED on/off as per new flag
 }
 
-void c_irq_identify_and_clear_source(void) {
 
-	ARMTIMER->Clear = 1;											// Write any value to register to clear irq ... PAGE 198
-
-	/* As we are running nested interrupts we must clear the Pi Irq controller,
-	the timer irq pending is bit 0 of pending register 1 as it's irq 0 */
-	IRQ->IRQPending1 &= ~0x1;										// Clear timer pending irq bit 0
-}
 
 static const char Spin[4] = { '|', '/', '-', '\\' };
 static int i = 0;
 int main (void) {
-	if (PiConsole_Init(1280, 1024, 16)) {
-		printf("SmartStart compiled for Arm%d, AARCH%d with %u core s/w support\n",
-			RPi_CompileMode.CodeType, RPi_CompileMode.AArchMode * 32 + 32,
-			(unsigned int)RPi_CompileMode.CoresSupported);							// Write text
-		printf("Detected %s CPU, part id: 0x%03X, Cores made ready for use: %u\n",
-			RPi_CpuIdString(), RPi_CpuId.PartNumber, (unsigned int)RPi_CoresReady);	// Write text
-		printf("Pi IO base address %08X\n", (unsigned int)RPi_IO_Base_Addr);			// Write text
-		printf("Pi booted from address %08X\n", (unsigned int)RPi_BootAddr);			// Write text
-	}
+	PiConsole_Init(0, 0, 0, &printf);								// Auto resolution console, show resolution to screen
+	displaySmartStart(&printf);										// Display smart start details
+	ARM_setmaxspeed(&printf);										// ARM CPU to max speed and confirm to screen
 
-	/* Enable the timer interrupt IRQ */
-	IRQ->EnableBasicIRQs.Enable_Timer_IRQ = true;
-
-
-	/* Setup the system timer interrupt */
-	ARMTIMER->Load = 0x400;
-
-	/* Setup the ARM Timer */
-	ARMTIMER->Control.Counter32Bit = true;
-	ARMTIMER->Control.Prescale = Clkdiv256;
-	ARMTIMER->Control.TimerIrqEnable = true;
-	ARMTIMER->Control.TimerEnable = true;
+	TimerIrqHandler oldHandler = TimerIrqSetup(1000000, c_irq_handler);
+	printf("Old handler %08x\n", (uintptr_t)oldHandler);
 
 	/* Enable interrupts! */
 	EnableInterrupts();
+
 
 	while (1) {
 		printf("Deadloop %c\r", Spin[i]);
