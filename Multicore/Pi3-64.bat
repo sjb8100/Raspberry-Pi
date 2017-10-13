1a0724c6b4d7bf8c54360d7f560a7b0bf065d28b
Leon de Boer <ldeboer@gateway.net.au>
2017-10-13 14:56:33 +0800

    Update to smartstart 2.03

--- Multicore/Pi3-64.bat
+++ Multicore/Pi3-64.bat
@@ -1,19 +1,19 @@
 @REM COMPILER COMMAND LINE
 @echo off
-set "bindir=g:\pi\gcc_linaro_6_3\bin\"
-set "cflags=-Wall -O2 -flto -march=armv8-a -mtune=cortex-a53 -mlittle-endian -mabi=lp64 -nostdlib -nostartfiles -ffreestanding"
-set "sflags=-fomit-frame-pointer -fno-common -fno-asynchronous-unwind-tables"
-set "linkflags=-Wl,--print-gc -Wl,-Bdynamic -Wl,-Map,kernel.map -Wl,--build-id=none"
-set "outflags=-Wa,-a=list.txt -o kernel.elf"
-set "libflags=-lc -lg -lm -lgcc"
+set "bindir=g:\pi\gcc_linaro_7_1\bin\"
+set "cpuflags=-Wall -O3 -march=armv8-a+simd -mtune=cortex-a53 -mstrict-align -fno-tree-loop-vectorize -fno-tree-slp-vectorize"
+set "asmflags=-nostdlib -nostartfiles -ffreestanding -fno-asynchronous-unwind-tables -fomit-frame-pointer -Wa,-a>list.txt"
+set "linkerflags=-Wl,-gc-sections -Wl,--build-id=none -Wl,-Bdynamic -Wl,-Map,kernel.map"
+set "outflags=-o kernel.elf"
+set "libflags=-lc -lm -lgcc"
 @echo on
-%bindir%aarch64-elf-gcc %cflags% %sflags% %linkflags% -Wl,-T,rpi64.ld main.c  SmartStart64.S rpi-BasicHardware.c %outflags% %libflags% 
+%bindir%aarch64-elf-gcc.exe %cpuflags% %asmflags% %linkerflags% -Wl,-T,rpi64.ld main.c SmartStart64.S rpi-SmartStart.c emb-stdio.c %outflags% %libflags% 
 @echo off
 if %errorlevel% EQU 1 (goto build_fail)
 
 @REM LINKER COMMAND LINE
 @echo on
-%bindir%aarch64-elf-objcopy kernel.elf -O binary kernel8.img
+%bindir%aarch64-elf-objcopy kernel.elf -O binary DiskImg\kernel8.img
 @echo off
 if %errorlevel% EQU 1 (goto build_fail) 
 echo BUILD COMPLETED NORMALLY
--- Multicore/main.c
+++ Multicore/main.c
@@ -1,9 +1,8 @@
 #include <stdbool.h>		// C standard needed for bool
 #include <stdint.h>			// C standard for uint8_t, uint16_t, uint32_t etc
-#include <stdio.h>			// Needed for printf
 #include <stdlib.h>			// Needed for rand
 #include "rpi-smartstart.h"
-#include "rpi-BasicHardware.h"
+#include "emb-stdio.h"
 
 static int idx = 0;
 static int core = 0;
@@ -39,28 +38,13 @@ void c_irq_handler (void) {
 	}
 }
 
-void c_irq_identify_and_clear_source (void) {
-	/* Clear the ARM Timer interrupt - it's the only interrupt we have enabled, 
-	   so we want don't have to work out which interrupt source caused us to interrupt */
-	ARMTIMER->Clear = 1;											// Write any value to register to clear irq ... PAGE 198
-
-	/* As we are running nested interrupts we must clear the Pi Irq controller,
-	   the timer irq pending is bit 0 of pending register 1 as it's irq 0 */
-	IRQ->IRQPending1 &= ~0x1;										// Clear timer pending irq bit 0
-}
 
 CORECALLFUNC test;
 
 int main (void) {
-
-	PiConsole_Init(1280, 1024, 16);
-	printf("SmartStart compiled for Arm%d, AARCH%d with %u core s/w support\n",
-		RPi_CompileMode.CodeType, RPi_CompileMode.AArchMode * 32 + 32,
-		(unsigned int)RPi_CompileMode.CoresSupported);							// Write text
-	printf("Detected %s CPU, part id: 0x%03X, Cores made ready for use: %u\n",
-		RPi_CpuIdString(), RPi_CpuId.PartNumber, (unsigned int)RPi_CoresReady);	// Write text
-	printf("Pi IO base address %08X\n", (unsigned int)RPi_IO_Base_Addr);			// Write text
-	printf("Pi booted from address %08X\n", (unsigned int)RPi_BootAddr);			// Write text
+	PiConsole_Init(0, 0, 0, printf);								// Auto resolution console, message to screen
+	displaySmartStart(printf);										// Display smart start details
+	ARM_setmaxspeed(printf);										// ARM CPU to max speed no message to screen
 
 	volatile int i = 0;
 	while (i < 100000) i++;   // My printf is not re-entrant so small delay
@@ -74,22 +58,10 @@ int main (void) {
 	i = 0;
 	while (i < 100000) i++;   // My printf is not re-entrant so small delay
 
+	TimerIrqSetup(500000, c_irq_handler);							// Give me flashing LED and timer based hyperthread launch 
+	EnableInterrupts();												// Start interrupts rolling
 
-    /* Enable the timer interrupt IRQ */
-	IRQ->EnableBasicIRQs.Enable_Timer_IRQ = true;
 
-
-    /* Setup the system timer interrupt */
-	ARMTIMER->Load = 0x400;
-
-    /* Setup the ARM Timer */
-	ARMTIMER->Control.Counter32Bit = true;
-	ARMTIMER->Control.Prescale = Clkdiv256;
-	ARMTIMER->Control.TimerIrqEnable = true;
-	ARMTIMER->Control.TimerEnable = true;
-
-    /* Enable interrupts! */
-	EnableInterrupts();
 
 	
 	while (1) {
