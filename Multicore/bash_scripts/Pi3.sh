982e45a0ebf650ae594a4cb187eb4b6162b01e7d
Leon de Boer <ldeboer@gateway.net.au>
2017-10-13 15:05:12 +0800

    Delete Pi3.sh

--- Multicore/bash_scripts/Pi3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc-6 -Wall -O2 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv8-a -mtune=cortex-a53 -nostdlib -nostartfiles -ffreestanding -fno-common -Wl,-gc-sections -Wl,--build-id=none -Wl,-T,rpi32.ld main.c  SmartStart32.S rpi-BasicHardware.c printf.c -o kernel.elf -lc -lm -lgcc
-objcopy kernel.elf -O binary kernel8-32.img
