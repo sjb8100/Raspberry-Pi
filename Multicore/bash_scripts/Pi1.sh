bb7a80b72fa7b81654b6280ca1fe94854a97d4a2
Leon de Boer <ldeboer@gateway.net.au>
2017-10-13 15:04:57 +0800

    Delete Pi1.sh

--- Multicore/bash_scripts/Pi1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc-6 -Wall -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostdlib -nostartfiles -ffreestanding -fno-common -Wl,-gc-sections -Wl,--build-id=none -Wl,-T,rpi32.ld main.c smartstart32.S rpi-BasicHardware.c printf.c -o kernel.elf -lc -lm -lgcc
-objcopy kernel.elf -O binary kernel.img
