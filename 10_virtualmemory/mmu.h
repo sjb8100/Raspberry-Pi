f8a12140ab90e17f40fba4d254a79dbc531bf685
Leon de Boer <ldeboer@gateway.net.au>
2018-06-14 13:58:29 +0800

    Add files via upload

--- 10_virtualmemory/mmu.h
+++ 10_virtualmemory/mmu.h
@@ -1,30 +1,20 @@
-/*
- * Copyright (C) 2018 bzt (bztsrc@github)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
+#ifndef _MMU_
+#define _MMU_
+
+#ifdef __cplusplus								// If we are including to a C++
+extern "C" {									// Put extern C directive wrapper around
+#endif
+#include <stdint.h>								// Needed for uint8_t, uint32_t, etc
+
 extern uint32_t table_loaded;
 
 void init_page_table (void);
 
 void mmu_init (void);
 uint64_t virtualmap(uint32_t phys_addr);
+
+#ifdef __cplusplus								// If we are including to a C++ file
+}												// Close the extern C directive wrapper
+#endif
+
+#endif
